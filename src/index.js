bc55daa3beb3413f4a82e25241c62a84e3379b9b
Lawrence Rama <oleng_rama@hotmail.com>
2024-04-28 08:06:38 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { loadRemoteModule } from "oleng-mfe-utils";
+const ErrorMessage = () => <div>Failed to load remote module</div>;
+
+const App = () => {
+  const [RemoteModule, setRemoteModule] = useState(null);
+  const [hasError, setHasError] = useState(false);
+
+  async function init() {
+    try {
+      const remoteModule = await loadRemoteModule({
+        url: "http://localhost:8081/remoteEntry.js",
+        scope: "microfrontend",
+        module: "./Microfrontend",
+      });
+      setRemoteModule(() => remoteModule);
+    } catch (error) {
+      console.error("Failed to load remote module", error);
+      setHasError(true);
+    }
+  }
+
+  // Load the remote module when the component mounts
+  React.useEffect(() => {
+    init();
+  }, []);
+
+  if (hasError) {
+    return <ErrorMessage />;
+  }
+
+  const MFE1 = RemoteModule ? RemoteModule.default : null;
+
+  return (
+    <div>
+      <h1>Container</h1>
+      <div>{MFE1 ? <MFE1 /> : "Loading remote module..."}</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    proxy: [
+      {
+        context: ["/jsmfe"],
+        target: "http://localhost:3000",
+        secure: false,
+        changeOrigin: true,
+      },
+    ],
+  },
+};
